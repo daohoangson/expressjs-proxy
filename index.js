e56f09ddee1bbce813152b41123163f8afe697c6
Dao Hoang Son <daohoangson@gmail.com>
2021-08-04 02:19:11 +0700

    First version

---
+++ index.js
@@ -0,0 +1,54 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const fetch = require('node-fetch')
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.text({ type: '*/*' }))
+
+app.post('*', async (req, res) => {
+  console.time('proxy');
+
+  const { body: input, headers: { ['content-type']: inputContentType }, path } = req
+
+  /**
+   * Sending POST request to `/aHR0cHM6Ly9hcGlleC5qdW5vLnZuL29zcy1wb3MtZ2F0ZS0xL2d1cnUvY2FydF9jb25zdW1lcg==`
+   * means proxying to `https://apiex.juno.vn/oss-pos-gate-1/guru/cart_consumer`
+   */
+  const url = Buffer.from(path.substr(1), 'base64').toString('ascii')
+  if (!url.match(/https?:\/\//)) {
+    return res.sendStatus(404)
+  }
+
+  const inputHeaders = {}
+  if (inputContentType) {
+    inputHeaders['Content-Type'] = inputContentType
+  }
+  console.log('input', url, inputHeaders, input)
+
+  const remote = await fetch(
+    url,
+    {
+      method: 'POST',
+      headers: inputHeaders,
+      body: input,
+    }
+  )
+  const output = await remote.text()
+  const { status, headers } = remote
+
+  const outputContentType = headers.get('content-type')
+  if (outputContentType) {
+    res.setHeader('Content-Type', outputContentType)
+  }
+
+  res.status(status).send(output)
+  console.log('output', status, outputContentType, output)
+
+  console.timeEnd('proxy');
+})
+
+app.listen(port, () => {
+  console.log(`proxy app listening at http://localhost:${port}`)
+})
